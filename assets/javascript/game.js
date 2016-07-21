1f855791cd603598ecf44553af430779aa6e01be
Cortney Gobern <CortneyGobern@Cortneys-MacBook-Pro.local>
2016-07-20 23:48:30 -0400

    added fading feature

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,8 @@
 //this means your javascript is working if 'test' shows up in your console log
 console.log('test')
+
+//allows heading to fade in once documnet is ready
+$(document).ready(function () {
+	$("#mainHeading").fadeIn(3000);
+	console.log("this is working");
+});
