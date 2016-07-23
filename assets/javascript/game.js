15cea98b396e4f6bc4e78a12494a3914b6561868
Cortney Gobern <CortneyGobern@Cortneys-MacBook-Pro.local>
2016-07-23 16:42:40 -0400

    images updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,45 @@ console.log('test')
 
 //allows heading to fade in once documnet is ready
 $(document).ready(function () {
-	$("body").fadeIn(3000);
-	console.log("this is working");
-});
-
-$(document).ready(function () {
 	var characters = new Array(4);
-	characters[0] = new Character ('Aang', 'aang.jpg', 56, 1, 93);
-	characters[1] = new Character ('Toph', 'toph.jpg', 40, 12, 110);
-	characters[2] = new Character ('Zuko', 'Zuko.png', 32, 3, 150);
-	characters[3] = new Character ('Katara', 'katara.jpg', 4, 121, 15);
+	characters[0] = new Character ('Aang', 93, 1, 56);
+	characters[1] = new Character ('Toph', 110, 12, 40);
+	characters[2] = new Character ('Zuko', 150, 3, 32);
+	characters[3] = new Character ('Katara', 121, 4, 15);
+
+	var hitpoints = [120, 130, 140, 150];
+
+	var fighter = -1;
+	var nemesis = -1;
+	var losingRow = new Array;
+	var roundOver = false;
+	var numberOfWins = 0;
+
+	function fighter(name, image, health, attack, counter) {
+		this.name = name;
+		this.image = image;
+		this.health = health;
+		this.attack = attack;
+		this.counter = counter;
+		this.status = 'available';
+	}
+
+	function showFighterPool () {
+		$('#fighters').empty();
+		for (ctr = 0; ctr < fighters.length)
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
